4442e2815e2164df45d61aeeeb67f3b3c59b57b6
J4yLo <jason.george@students.plymouth.ac.uk>
2021-11-21 21:42:12 +0000

    Finalisation on the feature implementation of make a bookings page

--- app/src/main/java/com/example/farmbnb/MakeABooking.java
+++ app/src/main/java/com/example/farmbnb/MakeABooking.java
@@ -9,6 +9,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MakeABooking extends AppCompatActivity {
@@ -24,8 +25,10 @@ public class MakeABooking extends AppCompatActivity {
     AutoCompleteTextView DropDownMenu;
     ArrayAdapter<String> Adapter;
 
-    private Button btnBack;
+    private Button btnBack2;
+    private Button btnSelectBooking;
     private Boolean ItemValidator = false;
+    private Button btnLogOut2;
 
 
 
@@ -35,6 +38,10 @@ public class MakeABooking extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_abooking);
 
+        btnBack2 = findViewById(R.id.btnBack2);
+        btnLogOut2 = findViewById(R.id.btnLogOut2);
+        btnSelectBooking = findViewById(R.id.btnSelectBooking);
+
         Adapter = new ArrayAdapter<>(this, R.layout.dropdownmenu,Accommodations);
         DropDownMenu = findViewById(R.id.DropDownMenu);
 
@@ -42,6 +49,8 @@ public class MakeABooking extends AppCompatActivity {
 
         DropDownMenu.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
+
+            // Code that changes boolean value to true, this is to validate if a user actually chose an item before they click a button
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 if (parent.getItemAtPosition(position).equals("Please Select Accommodation Type"))
                 {
@@ -56,7 +65,8 @@ public class MakeABooking extends AppCompatActivity {
         });
 
 
-        btnBack.setOnClickListener(new View.OnClickListener() {
+        // Code that takes user to set dates page if they have an Accommodation selected
+        btnSelectBooking.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v)
             {
@@ -65,10 +75,39 @@ public class MakeABooking extends AppCompatActivity {
                     Intent MakeABooking2 = new Intent(MakeABooking.this, MakeABooking2.class);
                     startActivity(MakeABooking2);
                 }
+                else
+                {
+                    Toast.makeText(MakeABooking.this, "Please Select An Accommodation", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        // Code that returns user to login page
+        btnLogOut2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLoginPage();
+            }
+        });
+
+        btnBack2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                openHomePage();
             }
         });
 
 
+    }
 
+    public void openLoginPage(){
+        Intent MainActivity = new Intent(MakeABooking.this, MainActivity.class);
+        startActivity(MainActivity);
+    }
+
+    public void openHomePage(){
+        Intent HomePage = new Intent(MakeABooking.this, HomePage.class);
+        startActivity(HomePage);
     }
 }
