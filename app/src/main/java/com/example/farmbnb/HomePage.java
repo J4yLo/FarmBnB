b5a64b5b7c471fef37b9d6884b81dee769da20ba
J4yLo <jason.george@students.plymouth.ac.uk>
2021-11-21 20:50:09 +0000

    DropDown Menu Layout and implementation on bookings page

--- app/src/main/java/com/example/farmbnb/HomePage.java
+++ app/src/main/java/com/example/farmbnb/HomePage.java
@@ -2,13 +2,82 @@ package com.example.farmbnb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomePage extends AppCompatActivity {
+
+
+    private Button btnViewAndManageBookings;
+    private Button btnMakeABooking;
+    private Button btnLogOut;
+    private Button btnViewBookingHistory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+
+
+        btnViewAndManageBookings = findViewById(R.id.btnViewAndManageBookings);
+        btnMakeABooking = findViewById(R.id.btnMakeABooking);
+        btnLogOut = findViewById(R.id.btnLogOut);
+        btnViewBookingHistory = findViewById(R.id.btnViewBookingHistory);
+
+        // Code that returns user to login page
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLoginPage();
+            }
+        });
+
+        // Code that takes the user to the view and manage bookings page
+        btnViewAndManageBookings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openViewAndManageBookingsPage();
+            }
+        });
+
+        // Code that takes the user to the make a booking page
+        btnMakeABooking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MakeABooking();
+            }
+        });
+
+        btnViewBookingHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ViewHistory();
+            }
+        });
+
+    }
+
+
+    public void openLoginPage(){
+        Intent MainActivity = new Intent(HomePage.this, MainActivity.class);
+        startActivity(MainActivity);
+    }
+
+    public void openViewAndManageBookingsPage(){
+        Intent ViewAndManageBookings = new Intent(HomePage.this, ViewAndManageBookings.class);
+        startActivity(ViewAndManageBookings);
+    }
+
+    public void MakeABooking(){
+        Intent MakeABooking = new Intent(HomePage.this, MakeABooking.class);
+        startActivity(MakeABooking);
+    }
+
+    public void ViewHistory(){
+        //Intent MakeABooking = new Intent(HomePage.this, MakeABooking.class);
+        //startActivity(MakeABooking);
     }
 }
--- app/src/main/java/com/example/farmbnb/MakeABooking.java
+++ app/src/main/java/com/example/farmbnb/MakeABooking.java
@@ -3,12 +3,44 @@ package com.example.farmbnb;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+
 
 public class MakeABooking extends AppCompatActivity {
+
+    String[] Accommodations = {
+            "Farm House Bedroom 1",
+            "Farm House Bedroom 2",
+            "Farm House Bedroom 3",
+            "Barn - Self Catering",
+            "Hut - Self Catering",
+            "Hut - Catering Included",
+    };
+    AutoCompleteTextView DropDownMenu;
+    ArrayAdapter<String> Adapter;
+
+    private Button btnBack;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_abooking);
+
+        Adapter = new ArrayAdapter<>(this, R.layout.dropdownmenu,Accommodations);
+        DropDownMenu = findViewById(R.id.DropDownMenu);
+
+        DropDownMenu.setAdapter(Adapter);
+
+
+
+
+
     }
 }
